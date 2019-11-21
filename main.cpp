dfbd5b0cef8073af3e60eee9c5ac36e9b5953c81
Hubert Cwajda <cwajdahubert@gmail.com>
2019-11-21 09:42:31 +0100

    post final

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 #include "fileReader.hpp"
 #include "instanceFactory.hpp"
 #include <chrono>
@@ -6,16 +7,29 @@
 #include "greedy.hpp"
 
 int main(int argv, char *argc[]) {
-//    auto p1 = std::chrono::steady_clock::now();
+    auto p1 = std::chrono::steady_clock::now();
 
-    int amount = argv > 2 ? std::stoi(argc[2]) : -1;
-    instanceFactory factory{argc[1], amount};
+    int amount = argv > 3 ? std::stoi(argc[3]) : -1;
+    instanceFactory factory{argc[2], amount};
     auto tasks = factory.generate();
     auto procs = factory.maxProcessors();
-
     auto res = greedy(tasks, procs);
-    res.print();
-//    auto p2 = std::chrono::steady_clock::now();
-//    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(p2 - p1).count();
+
+    auto p2 = std::chrono::steady_clock::now();
+
+
+    switch (argc[1][0])
+    {
+        case 'p':
+            res.print();
+            break;
+        case 't':
+            std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(p2 - p1).count();
+            break;
+        case 'l':
+            std::cout << res.length();
+            break;
+    }
+
     return 0;
 }
